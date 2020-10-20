bbee1b0275503874d4e79ad1c073e56f1c892063
Fitrah Aditiya <50580834+raiz3n19@users.noreply.github.com>
2020-10-20 21:19:01 +0700

    Update config.ts

--- config.ts
+++ config.ts
@@ -2,20 +2,20 @@ module.exports = {
   siteMetadata: {
     copyright: "© All rights reserved.",
     description:
-      "Róbert is traveling the world with his laptop and shipping products for the web, while constantly embracing new ideas. Come, and join him on this journey!",
+      "Catatan Harian si Adit",
     googleAnalyticsId: "UA-73379983-2",
     pathPrefix: "/",
     siteUrl: "https://robertistok.com",
-    title: "Róbert Istók",
+    title: "Catatan Harian si Adit",
     author: {
-      birthDate: new Date("1994-07-05"),
-      firstname: "Róbert",
-      lastname: "Istók",
-      name: "Róbert Istók",
-      title: "Javascript Engineer",
+      birthDate: new Date("2000-28-12"),
+      firstname: "Fitrah",
+      lastname: "Aditiya",
+      name: "Fitrah Aditiya",
+      title: "Catatan Harian si Adit",
       descriptions: {
         intro:
-          "I am a developer and a dreamer, passionate about freeing up energy through code and healthy habits.",
+          "Welcome to my personal blog! hope you Enjoy here!!",
       },
       assets: {
         icon: "robertistok_avatar_rounded.png",
@@ -24,13 +24,9 @@ module.exports = {
       resume: "robert_istok_resume.pdf",
       username: "robertistok",
       social: {
-        email: "robertistok94@gmail.com",
-        github: "robertistok",
-        goodreads: "robertistok",
-        instagram: "robertistok",
-        linkedin: "robertistok",
-        medium: "robertistok",
-        twitter: "robertistok",
+        email: "popmonst3r@gmail.com",
+        github: "raiz3n19",
+        instagram: "faditiya_",
       },
     },
   },
